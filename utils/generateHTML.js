1796c5ad2185b2b5b6defef9853c83ac3189eb4b
jennahopeclem <jennahopeclem@gmail.com>
2023-01-02 17:46:48 -0500

    bootstrap styling added

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -8,32 +8,39 @@ function createHTML(team) {
   <meta name="Description" content="Enter your description here"/>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-  <link rel="stylesheet" href="/dist/style.css">
+  <link rel="stylesheet" href="./style.css">
   <title>Team Profile Generator</title>
   </head>
   <body>
-    
+
   ${team
     .map((member) => {
       return ` 
-  <div class="card" style="width: 18rem;">
-    <div class="container">
-      <h2>${member.name}, ${member.getRole(
+  <div class="row my-5 align-items-center justify-content-center">
+    <div class="col-8 col-lg-4 col-xl-3">
+      <div class="card border-primary">
+        <div class="card-body text-left py-4">
+              <h5 class="card-title">${member.name}, ${member.getRole(
         "Manager",
         "Engineer",
         "Intern"
-      )}</h2>
-      <h3 class="card-subtitle mb-2 text-muted">ID: ${member.id}</h3>
-    <p class="card-link">Email: <a href="mailto:${
-      member.email
-    }" target="_blank">${member.email}</a></p>
-    <p class="card-link">${member.getSpecial(
-      "Manager",
-      "Engineer",
-      "Intern"
-    )}</p>
-  </div>
-</div>`;
+      )}</h5>
+              <p class="card-text">
+                ID: ${member.id}
+              </p>
+              <p class="card-link">Email: <a href="mailto:${
+                member.email
+              }" target="_blank">${member.email}</a></p>
+              <p class="card-link">${member.getSpecial(
+                "Manager",
+                "Engineer",
+                "Intern"
+              )}</p>
+              </p>
+              </div>
+              </div>
+            </div>
+          </div>`;
     })
     .join("")}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
